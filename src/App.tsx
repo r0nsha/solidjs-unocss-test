8a3690b044786950438df47fb816ceb2ee5c85c6
r0nsha <r0nsh4v1t@gmail.com>
2023-04-09 23:30:50 +0300

    add web fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import type { Component } from "solid-js"
 
 export const App: Component = () => {
 	return (
-		<p class="text-(4xl green-700 center) py-20">
-			Hello{" "}
-			<a
-				class="text-pink-600 hover:(font-bold hover:border-1)"
-				href="https://antfu.me/posts/reimagine-atomic-css"
-				target="atomic-css"
-			>
-				Atomic CSS
-			</a>
-			!
-		</p>
+		<div>
+			<p class="text-(4xl green-700 center) py-20 font-sans">
+				Hello{" "}
+				<a
+					class="text-pink-600 hover:(font-bold hover:border-1)"
+					href="https://antfu.me/posts/reimagine-atomic-css"
+					target="atomic-css"
+				>
+					3 x 12 @ 20kg
+				</a>
+				!
+			</p>
+			<p class="text-(5xl center) font-(sans bold)">Setup: web application</p>
+			<p class="text-(5xl center) font-(sans bold)">hare</p>
+		</div>
 	)
 }
--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig, presetUno, transformerVariantGroup } from "unocss"
+import { defineConfig, presetUno, presetWebFonts, transformerVariantGroup } from "unocss"
 
 export default defineConfig({
-	presets: [presetUno()],
+	presets: [
+		presetUno(),
+		presetWebFonts({
+			provider: "google",
+			fonts: {
+				sans: "DM Sans",
+				mono: "Noto Sans Mono",
+			},
+		}),
+	],
 	transformers: [transformerVariantGroup()],
 })
