d57e8c97e5415633b8738bb7b086f72e148e5c9a
r0nsha <r0nsh4v1t@gmail.com>
2023-04-11 01:25:10 +0300

    add tabler icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Component } from "solid-js"
-import { ThemeProvider, useThemeContext } from "./components/theme/theme.context"
+import { ThemeProvider } from "./components/theme/theme.context"
 import { ThemeSelect } from "./components/theme/theme-select"
+import { IconArrowRight } from "@tabler/icons-solidjs"
 
 export const App: Component = () => {
 	return (
 		<ThemeProvider>
 			<div>Hello</div>
+			<IconArrowRight class="color-primary-500" size={48} />
 			<ThemeSelect />
 		</ThemeProvider>
 	)
