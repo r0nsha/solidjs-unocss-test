a8cbf2e45768bb0c5893f1b3a9014d637425c714
r0nsha <r0nsh4v1t@gmail.com>
2023-04-15 01:08:39 +0300

    wip

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,12 +1,26 @@
-import { Component } from "solid-js"
+import { Component, JSX } from "solid-js"
 import { useI18n } from "../../locale/i18n.context"
 import { IconComponent } from "../../types/component"
-import { IconSettings } from "@tabler/icons-solidjs"
+import { IconSettings, IconTriangleInverted, IconTriangleInvertedFilled } from "@tabler/icons-solidjs"
 
 export const Sidebar: Component = () => {
+	const [t] = useI18n()
+
 	return (
-		<div class="w-60 flex flex-col border-e-(2 solid surface-100)">
-			<Item icon={IconSettings} />
+		<div class="w-60 border-e-(2 solid surface-100)">
+			<Title />
+			<div class="flex-1 flex flex-col mx-1">
+				<Item icon={IconSettings} text={t.settings()} suffix={<IconTriangleInvertedFilled size={10} />} />
+			</div>
+		</div>
+	)
+}
+
+const Title: Component = () => {
+	return (
+		<div class="h-11 flex items-center gap-2.5 px-3">
+			<div class="w-5.5 h-5.5 bg-primary-200 rounded-1" />
+			<span class="text-sm font-bold">Your name here...</span>
 		</div>
 	)
 }
@@ -14,8 +28,15 @@ export const Sidebar: Component = () => {
 type ItemProps = {
 	icon: IconComponent
 	text: string
+	suffix?: JSX.Element
 }
 
 const Item: Component<ItemProps> = (props) => {
-	return <div class="px-3 py-2">{props.icon({ size: 22 })}</div>
+	return (
+		<div class="px-2 py-1 rounded-1.5 text-sm font-bold flex items-center gap-2.5 select-none color-on-secondary transition-colors duration-100 hover:bg-surface-100 active:bg-surface-200">
+			{props.icon({ size: 22 })}
+			<span class="flex-1">{props.text}</span>
+			{props.suffix}
+		</div>
+	)
 }
