e00894dc24ee64bdc0cc4f42c5718b2445a680b6
r0nsha <r0nsh4v1t@gmail.com>
2023-04-15 01:44:12 +0300

    divider

---
+++ src/ui/divider.tsx
@@ -0,0 +1,25 @@
+import classNames from "classnames"
+import { Component, mergeProps } from "solid-js"
+
+export type DividerOrientation = "horizontal" | "vertical"
+
+export type DividerProps = {
+	class?: string
+	orientation?: DividerOrientation
+}
+
+export const Divider: Component<DividerProps> = (_props) => {
+	const props = mergeProps({ orientation: "horizontal" } satisfies DividerProps, _props)
+
+	return (
+		<div
+			class={classNames("bg-surface-100", props.class)}
+			classList={{
+				"w-full": props.orientation === "horizontal",
+				"h-0.35": props.orientation === "horizontal",
+				"w-0.35": props.orientation === "vertical",
+				"h-full": props.orientation === "vertical",
+			}}
+		/>
+	)
+}
