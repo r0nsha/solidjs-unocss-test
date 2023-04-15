9a2565797817190a400fe9d62110c864932d50c6
r0nsha <r0nsh4v1t@gmail.com>
2023-04-15 16:23:14 +0300

    revamp outline button styles

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -38,16 +38,16 @@ const variantClasses: Record<ButtonVariant, Record<ColorScheme | "disabled", str
 	},
 	outline: {
 		primary:
-			"bg-transparent color-primary-500 outline-(width-2 solid primary-500 offset--2) hover:bg-primary-50 active:(bg-primary-100 color-primary-600 outline-primary-600) focus-visible:(bg-primary-50)",
+			"bg-transparent color-primary-500 outline-(width-2 solid primary-500 offset--2) hover:(bg-primary-50 color-primary-600 outline-primary-600) active:(bg-primary-100 color-primary-700 outline-primary-700) focus-visible:(bg-primary-50 color-primary-600 outline-primary-600)",
 		neutral:
-			"bg-transparent color-on-secondary outline-(width-2 solid on-secondary offset--2) hover:bg-surface-100 active:(bg-surface-200 color-on-primary outline-on-primary) focus-visible:(bg-surface-200)",
+			"bg-transparent color-on-secondary outline-(width-2 solid on-secondary offset--2) hover:bg-surface-100 active:(bg-surface-200 color-on-primary outline-on-primary) focus-visible:(bg-surface-200 color-on-primary outline-on-primary)",
 		success:
-			"bg-transparent color-success-500 outline-(width-2 solid success-500 offset--2) hover:bg-success-50 active:(bg-success-100 color-success-600 outline-success-600) focus-visible:(bg-success-50)",
+			"bg-transparent color-success-500 outline-(width-2 solid success-500 offset--2) hover:(bg-success-50 color-success-600 outline-success-600) active:(bg-success-100 color-success-700 outline-success-700) focus-visible:(bg-success-50 color-success-600 outline-success-600)",
 		warning:
-			"bg-transparent color-warning-500 outline-(width-2 solid warning-500 offset--2) hover:bg-warning-50 active:(bg-warning-100 color-warning-600 outline-warning-600) focus-visible:(bg-warning-50)",
-		info: "bg-transparent color-info-500 outline-(width-2 solid info-500 offset--2) hover:bg-info-50 active:(bg-info-100 color-info-600 outline-info-600) focus-visible:(bg-info-50)",
+			"bg-transparent color-warning-500 outline-(width-2 solid warning-500 offset--2) hover:(bg-warning-50 color-warning-600 outline-warning-600) active:(bg-warning-100 color-warning-700 outline-warning-700) focus-visible:(bg-warning-50 color-warning-600 outline-warning-600)",
+		info: "bg-transparent color-info-500 outline-(width-2 solid info-500 offset--2) hover:(bg-info-50 color-info-600 outline-info-600) active:(bg-info-100 color-info-700 outline-info-700) focus-visible:(bg-info-50 color-info-600 outline-info-600)",
 		danger:
-			"bg-transparent color-danger-500 outline-(width-2 solid danger-500 offset--2) hover:bg-danger-50 active:(bg-danger-100 color-danger-600 outline-danger-600) focus-visible:(bg-danger-50)",
+			"bg-transparent color-danger-500 outline-(width-2 solid danger-500 offset--2) hover:(bg-danger-50 color-danger-600 outline-danger-600) active:(bg-danger-100 color-danger-700 outline-danger-700) focus-visible:(bg-danger-50 color-danger-600 outline-danger-600)",
 		disabled: "bg-transparent color-surface-300 outline-(width-2 solid surface-300 offset--2)",
 	},
 	ghost: {
@@ -70,7 +70,7 @@ export const Button: Component<ButtonProps> = (_props) => {
 	const props = mergeProps(
 		{
 			type: "button",
-			colorScheme: "primary",
+			colorScheme: "danger",
 		} satisfies Partial<ButtonProps>,
 		_props,
 	)
