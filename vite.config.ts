6d55fceccd8130721a50b4802d2de063960de967
r0nsha <r0nsh4v1t@gmail.com>
2023-04-22 12:04:59 +0300

    add bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
 import UnocssPlugin from "unocss/vite"
+import visualizer from "rollup-plugin-visualizer"
 
 export default defineConfig({
-	plugins: [solidPlugin(), UnocssPlugin()],
+	plugins: [solidPlugin(), UnocssPlugin(), visualizer()],
 	server: {
 		port: 3000,
 	},
