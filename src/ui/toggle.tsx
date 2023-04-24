cfb759bea13e3838e58a8fac124867b4cb93f344
r0nsha <r0nsh4v1t@gmail.com>
2023-04-24 12:45:40 +0300

    wip

--- src/ui/toggle.tsx
+++ src/ui/toggle.tsx
@@ -3,6 +3,8 @@ import { Component } from "solid-js"
 
 export type ToggleProps = {
 	checked?: boolean
+	// TODO:
+	// disabled?: boolean
 	onChange?: (newValue: boolean, event: Event) => void
 }
 
@@ -10,7 +12,7 @@ export const Toggle: Component<ToggleProps> = (props) => {
 	return (
 		<button
 			class={classNames(
-				"relative w-10 h-4.5 rounded-4.5 flex items-center transition-colors duration-100 outline-transparent border-none",
+				"relative w-10 h-4.5 rounded-4.5 flex items-center transition-(colors opacity) duration-100 outline-transparent border-none",
 				props.checked
 					? "bg-primary-500 hover:bg-primary-600 active:bg-primary-700"
 					: "bg-surface-300 hover:bg-surface-400 active:bg-surface-500",
