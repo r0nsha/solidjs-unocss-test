2f219a3ed3cc99f1c9afe414b863c8b3706ee6ac
r0nsha <r0nsh4v1t@gmail.com>
2023-04-24 14:52:43 +0300

    wip

--- src/routes/main/main.tsx
+++ src/routes/main/main.tsx
@@ -8,12 +8,16 @@ export const Main: Component = () => {
 		<div class="w-full h-full flex">
 			<Sidebar />
 			<div class="flex-1 flex flex-col">
-				<div class="h-10 px-4 flex items-center gap-4">
+				<div class="h-10 px-3 flex items-center gap-3">
 					<IconFileDescription size={20} />
-					<span class="text-lg font-bold">My Workout</span>
+					<span class="text-lg font-bold">Starting Strength</span>
 				</div>
 				<Divider />
-				<div class="px-14 py-8 overflow-y-auto flex-1">Write something...</div>
+				<div class="flex-1 py-8 flex justify-center overflow-y-auto">
+					<div class="w-full px-3 lg:(w-auto min-w-lg px-[5vw]) bg-surface-100 flex-shrink-1">
+						Write something...
+					</div>
+				</div>
 			</div>
 		</div>
 	)
