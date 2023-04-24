de5ebf058910b2988565aaeae85b51730a9b5eb6
r0nsha <r0nsh4v1t@gmail.com>
2023-04-24 14:05:51 +0300

    list item h-8

--- src/ui/list-item.tsx
+++ src/ui/list-item.tsx
@@ -26,7 +26,7 @@ export const ListItem: Component<ListItemProps> = (props) => {
 			{...html}
 			disabled={local.disabled}
 			class={classNames(
-				"h-7 px-2 py-0 bg-transparent border-none outline-transparent rounded-1 text-(sm start) flex flex-shrink-0 items-center gap-2 select-none color-on-secondary transition-(colors opacity) duration-100",
+				"h-8 px-2 py-0 bg-transparent border-none outline-transparent rounded-1 text-(sm start) flex flex-shrink-0 items-center gap-2 select-none color-on-secondary transition-(colors opacity) duration-100",
 				local.disabled
 					? "opacity-50"
 					: "hover:(bg-surface-hover color-on-primary) active:bg-surface-active focus-visible:bg-surface-hover",
