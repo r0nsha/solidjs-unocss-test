549cb095b1c20e475b207dfc4a252aac69ff8b60
r0nsha <r0nsh4v1t@gmail.com>
2023-04-25 15:12:03 +0300

    wip

--- src/routes/main/main.tsx
+++ src/routes/main/main.tsx
@@ -9,10 +9,10 @@ export const Main: Component = () => {
 			<Sidebar />
 			<div class="flex-1 flex lg:justify-center">
 				<div class="flex flex-col min-w-0 max-w-full lg:w-192 px-6 pt-5 overflow-y-auto">
-					<div class="mb-8">
+					<div class="mb-9">
 						<span class="color-on-secondary">Status line | Buttons | Last modified</span>
 					</div>
-					<div class="text-4xl font-bold mb-8">Starting Strength</div>
+					<div class="text-4xl font-bold mb-6">Starting Strength</div>
 					<div class="color-on-secondary">{t.workout_placeholder()}</div>
 				</div>
 			</div>
