9d4bc4a55b15d53f5ea9e3ab9adf800f24ee615f
r0nsha <r0nsh4v1t@gmail.com>
2023-04-28 18:36:20 +0300

    init radix colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,41 +1,15 @@
 import {
+	Preset,
 	defineConfig,
 	presetUno,
 	presetWebFonts,
 	transformerDirectives,
 	transformerVariantGroup,
 } from "unocss"
+import { RadixColors, presetRadix } from "unocss-preset-radix"
 
-const colors = ["neutral", "primary", "success", "info", "warning", "danger"] as const
-type Color = typeof colors[number]
-
-type ColorPalette = Record<string | number, string>
-
-const forEachColorLevel = (fn: (level: string | number) => void) => {
-	fn(50)
-	for (let i = 100; i < 1000; i += 100) {
-		fn(i)
-	}
-	fn(950)
-	fn("hover")
-	fn("active")
-}
-
-const colorPalette = (color: Color): ColorPalette => {
-	const palette: ColorPalette = {}
-	forEachColorLevel((level) => {
-		palette[level] = `var(--${color}-${level})`
-	})
-	return palette
-}
-
-const colorTheme = () => {
-	const t: Partial<Record<Color, ColorPalette>> = {}
-	colors.forEach((color) => {
-		t[color] = colorPalette(color)
-	})
-	return t
-}
+const aliases = ["neutral", "primary", "success", "info", "warning", "danger"] as const
+type Alias = typeof aliases[number]
 
 export default defineConfig({
 	presets: [
@@ -53,16 +27,19 @@ export default defineConfig({
 				},
 			},
 		}),
+		presetRadix({
+			palette: ["gray", "tomato", "grass", "blue", "amber", "red"],
+			aliases: {
+				neutral: "gray",
+				primary: "tomato",
+				success: "grass",
+				info: "blue",
+				warning: "amber",
+				danger: "red",
+			} satisfies Record<Alias, RadixColors>,
+			lightSelector: ":root, .theme-light",
+			darkSelector: ".theme-dark",
+		}) as unknown as Preset<{}>,
 	],
 	transformers: [transformerVariantGroup(), transformerDirectives()],
-	theme: {
-		colors: {
-			...colorTheme(),
-			on: {
-				primary: "var(--on-primary)",
-				secondary: "var(--on-secondary)",
-				dim: "var(--on-dim)",
-			},
-		},
-	},
 })
