5669b0bc8e7776a1e459fa072e3827b569533fa6
r0nsha <r0nsh4v1t@gmail.com>
2023-04-29 16:43:01 +0300

    remove button tests

--- src/routes/main/main.tsx
+++ src/routes/main/main.tsx
@@ -1,14 +1,11 @@
-import { Component, createSignal } from "solid-js"
+import { Component } from "solid-js"
 import { Sidebar } from "./sidebar/sidebar"
 import { useI18n } from "../../locale/i18n.context"
 import { Button } from "../../ui/button"
 import { IconArrowLeft, IconArrowRight, IconDotsVertical } from "@tabler/icons-solidjs"
-import { colors } from "../../types/color"
 
 export const Main: Component = () => {
 	const [t] = useI18n()
-
-	const [disabled, setDisabled] = createSignal(false)
 
 	return (
 		<div class="w-full h-full flex">
@@ -22,21 +19,8 @@ export const Main: Component = () => {
 					<span class="color-on-secondary text-sm select-none">Starting Strength</span>
 					<Button variant="ghost" color="neutral" small icon={IconDotsVertical} />
 				</div>
-				<div class="w-2xl flex flex-wrap gap-4">
-					{(["solid", "outline", "ghost"] as const).map((variant) =>
-						colors.map((color) => (
-							<Button
-								variant={variant}
-								color={color}
-								text="Button"
-								disabled={disabled()}
-								onClick={() => setDisabled((d) => !d)}
-							/>
-						)),
-					)}
-				</div>
 				<div class="flex-1 flex flex-col min-w-0 max-w-full lg:w-192 p-6 overflow-y-auto">
-					<div class="text-5xl font-bold mb-6">Starting Strength</div>
+					<div class="text-4xl font-bold mb-6">Starting Strength</div>
 					<div class="color-on-secondary">{t.workout_placeholder()}</div>
 				</div>
 			</div>
