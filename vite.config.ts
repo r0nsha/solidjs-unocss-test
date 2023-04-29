7d98eb8b15fd4a4e4cbea6011221b65532d8c2f2
r0nsha <r0nsh4v1t@gmail.com>
2023-04-29 19:49:24 +0300

    init tauri

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,18 @@ import visualizer from "rollup-plugin-visualizer"
 
 export default defineConfig({
 	plugins: [solidPlugin(), UnocssPlugin(), visualizer()],
+	clearScreen: false,
 	server: {
-		port: 3000,
+		port: 5173,
+		strictPort: true,
 	},
+	envPrefix: ["VITE_", "TAURI_"],
 	build: {
-		target: "esnext",
+		// Tauri uses Chromium on Windows and WebKit on macOS and Linux
+		target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
+		// don't minify for debug builds
+		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+		// produce sourcemaps for debug builds
+		sourcemap: !!process.env.TAURI_DEBUG,
 	},
 })
