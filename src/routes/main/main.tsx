50bff090ad1a6593d9ce79c748e1ca4fa4d8e5e9
r0nsha <r0nsh4v1t@gmail.com>
2023-04-29 19:19:33 +0300

    lighten header

--- src/routes/main/main.tsx
+++ src/routes/main/main.tsx
@@ -18,7 +18,7 @@ export const Main: Component = () => {
 						<Button variant="ghost" color="neutral" size="sm" icon={IconArrowLeft} />
 						<Button variant="ghost" color="neutral" size="sm" icon={IconArrowRight} />
 					</div>
-					<span class="color-neutral-12 text-sm select-none">Starting Strength</span>
+					<span class="color-neutral-11 text-sm select-none">Starting Strength</span>
 					<Button variant="ghost" color="neutral" size="sm" icon={IconDotsVertical} />
 				</div>
 				<div class="flex-1 flex flex-col min-w-0 max-w-full lg:w-224 p-6">
