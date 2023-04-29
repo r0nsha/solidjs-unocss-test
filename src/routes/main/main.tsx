0461460feb939e1938266b9e30964488dcf0a513
r0nsha <r0nsh4v1t@gmail.com>
2023-04-29 18:35:55 +0300

    remove button tests

--- src/routes/main/main.tsx
+++ src/routes/main/main.tsx
@@ -3,7 +3,6 @@ import { Sidebar } from "./sidebar/sidebar"
 import { useI18n } from "../../locale/i18n.context"
 import { Button } from "../../ui/button"
 import { IconArrowLeft, IconArrowRight, IconDotsVertical } from "@tabler/icons-solidjs"
-import { colors } from "../../types/color"
 import { Divider } from "../../ui/divider"
 
 export const Main: Component = () => {
@@ -21,11 +20,6 @@ export const Main: Component = () => {
 					</div>
 					<span class="color-neutral-11 text-sm select-none">Starting Strength</span>
 					<Button variant="ghost" color="neutral" size="sm" icon={IconDotsVertical} />
-				</div>
-				<div class="w-2xl flex flex-wrap gap-4">
-					{(["solid", "outline", "ghost"] as const).map((variant) =>
-						colors.map((color) => <Button variant={variant} color={color} text="Button" />),
-					)}
 				</div>
 				<div class="flex-1 flex flex-col min-w-0 max-w-full lg:w-192 p-6">
 					<div class="text-4xl font-bold mb-6">Starting Strength</div>
