d25bb2194a4f2135b95c4c885e3db86a76ffc9e7
r0nsha <r0nsh4v1t@gmail.com>
2023-04-29 16:47:34 +0300

    scrollbar

--- src/routes/main/main.tsx
+++ src/routes/main/main.tsx
@@ -3,6 +3,7 @@ import { Sidebar } from "./sidebar/sidebar"
 import { useI18n } from "../../locale/i18n.context"
 import { Button } from "../../ui/button"
 import { IconArrowLeft, IconArrowRight, IconDotsVertical } from "@tabler/icons-solidjs"
+import { colors } from "../../types/color"
 
 export const Main: Component = () => {
 	const [t] = useI18n()
@@ -10,8 +11,8 @@ export const Main: Component = () => {
 	return (
 		<div class="w-full h-full flex">
 			<Sidebar />
-			<div class="flex-1 flex flex-col lg:items-center">
-				<div class="h-10 self-stretch flex justify-between items-center px-2">
+			<div class="flex-1 flex flex-col lg:items-center overflow-y-auto">
+				<div class="sticky top-0 bg-neutral-1 h-10 self-stretch flex justify-between items-center px-2 flex-shrink-0">
 					<div class="flex items-center">
 						<Button variant="ghost" color="neutral" small icon={IconArrowLeft} />
 						<Button variant="ghost" color="neutral" small icon={IconArrowRight} />
@@ -19,7 +20,12 @@ export const Main: Component = () => {
 					<span class="color-on-secondary text-sm select-none">Starting Strength</span>
 					<Button variant="ghost" color="neutral" small icon={IconDotsVertical} />
 				</div>
-				<div class="flex-1 flex flex-col min-w-0 max-w-full lg:w-192 p-6 overflow-y-auto">
+				<div class="w-2xl flex flex-wrap gap-4">
+					{(["solid", "outline", "ghost"] as const).map((variant) =>
+						colors.map((color) => <Button variant={variant} color={color} text="Button" />),
+					)}
+				</div>
+				<div class="flex-1 flex flex-col min-w-0 max-w-full lg:w-192 p-6">
 					<div class="text-4xl font-bold mb-6">Starting Strength</div>
 					<div class="color-on-secondary">{t.workout_placeholder()}</div>
 				</div>
